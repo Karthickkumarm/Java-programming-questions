4b66c619c1a2007494729b611e53bbb55ec5dfaa
Karthick <karthickkumarmoorthys@gmail.com>
2025-02-14 09:03:22 +0530

    Practice question

--- src/RotateArray.java
+++ src/RotateArray.java
@@ -21,8 +21,8 @@ public class RotateArray {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        int k = 2;
+        int[] arr = {1, 2, 3, 4, 5,6,7};
+        int k = 3;
         
         leftRotate(arr, k);
         System.out.println(Arrays.toString(arr)); // Output: [3, 4, 5, 1, 2]
